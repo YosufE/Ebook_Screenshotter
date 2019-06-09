20480d7601532dad09bf63ef74de36645dd6967b
FlamingFury017 <yosufelsherif1234@gmail.com>
2019-06-09 23:25:44 +0200

    added feature to chose which page nr to begin and end with

--- screen.py
+++ screen.py
@@ -6,24 +6,24 @@ from pynput.keyboard import Key, Controller
 keyboard = Controller()
 
 
-time.sleep(4)
+time.sleep(5)
 
 
 # get screen dimensions
 
 
-def take_Screens(name, amount):
+def take_Screens(name, current_page, last_page):
     if not os.path.exists(name):
         os.makedirs(name)
-    for i in range(amount):
+    for i in range(last_page - current_page + 1):
         window = Gdk.get_default_root_window()
         x, y, width, height = window.get_geometry()
         pb = Gdk.pixbuf_get_from_window(window, x, y, width, height)
-        pb.savev(name+"/screenshot"+str(i)+".png", "png", (), ())
-        print("Screen Nr.:" + str(i))
+        pb.savev(name+"/page"+str(current_page+i)+".png", "png", (), ())
+        print("Page Nr.:" + str(current_page+i))
         keyboard.press(Key.right)
         keyboard.release(Key.right)
         time.sleep(2)
 
 
-take_Screens("Python 3 (Kaminski, Steffan)", 4)
+take_Screens("Python 3 (Kaminski, Steffan)", 1, 482)
